894cfec6685e11ba52085a8bec77c47f283d8a78
omarabdelrehim <omarabdelrehim8@gmail.com>
2023-09-10 02:31:45 +0200

    added movies details

--- js/script.js
+++ js/script.js
@@ -37,6 +37,63 @@ async function displayPopularMovies() {
   })
 }
 
+// Display movie details
+async function displayMovieDetails() {
+ const movieId = window.location.search.split("=")[1];
+ const movie = await fetchAPIData(`movie/${movieId}`)
+
+ const div = document.createElement("div");
+ div.innerHTML = `<div class="details-top">
+ <div>
+ ${
+  movie.poster_path 
+  ? `<img
+  src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+  class="card-img-top"
+  alt="${movie.title}"/>`
+
+  : `<img
+  src="images/no-image.jpg"
+  class="card-img-top"
+  alt="${movie.title}"
+/>`
+}
+
+ </div>
+ <div>
+   <h2>${movie.title}</h2>
+   <p>
+     <i class="fas fa-star text-primary"></i>
+     ${movie.vote_average.toFixed(1)} / 10
+   </p>
+   <p class="text-muted">Release Date: ${movie.release_date}</p>
+   <p>
+   ${movie.overview}
+   </p>
+   <h5>Genres</h5>
+   <ul class="list-group">
+     ${movie.genres.map(genre => `<li>${genre.name}</li>`).join("")}
+   </ul>
+   <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+ </div>
+</div>
+<div class="details-bottom">
+ <h2>Movie Info</h2>
+ <ul>
+   <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+   <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+   <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+   <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+ </ul>
+ <h4>Production Companies</h4>
+ <div class="list-group">${movie.production_companies.map(company => `<span>${company.name}</span>`).join(", ")}</div>
+</div>`
+
+ document.getElementById("movie-details").appendChild(div);
+
+ console.log(movie);
+}
+
 // Display 20 most popular tv shows
 async function displayPopularTVShows() {
   const {results} = await fetchAPIData("tv/popular");
@@ -106,6 +163,11 @@ function highlightActiveLink() {
   })
 }
 
+function addCommasToNumber(number) {
+  return number.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+}
+
+
 // Init App
 function init() {
   switch (global.currentPage) {
@@ -114,7 +176,7 @@ function init() {
       displayPopularMovies();
       break;
     case "/movie-details.html":
-      console.log("Movie Details");
+      displayMovieDetails();
       break;
     case "/search.html":
       console.log("Search");
